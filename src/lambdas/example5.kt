c275166cd9998fc84bd744bb23f2eec571d809d3
lucasxyz <lucasxyz9@gmail.com>
2024-10-25 19:29:33 -0300

    lambdas and high order functions

---
+++ src/lambdas/example5.kt
@@ -0,0 +1,20 @@
+package lambdas
+
+fun compareNumber(x: Int, y: Int, comparation: (Int, Int) -> Int): Int {
+    return comparation(x, y)
+}
+
+fun ordenPar(x: Int, y: Int, order: (Int,Int) -> Pair<Int,Int>) : Pair<Int,Int>{
+    return order(x,y)
+}
+
+fun main() {
+    val menor = compareNumber(5, 3) { a, b -> if (a <= b) a else b }
+    println("Menor: $menor")  // Imprime "Menor: 3"
+
+    val mayor = compareNumber(5, 3) {a,b  -> if (a >= b) a else b }
+    println("Mayor: $mayor")  // Imprime "Mayor: 5"
+
+    val ascendent = ordenPar(16,8){ a,b -> if ( a < b ) Pair(a,b) else Pair(b,a)}
+    println(ascendent)
+}
